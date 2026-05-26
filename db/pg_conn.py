3834a4864607b265b48626da31d936bfed7eab21
al1me0s7 <alime.osmanova@nure.ua>
2026-05-26 20:34:39 +0300

    fix render database

--- db/pg_conn.py
+++ db/pg_conn.py
@@ -1,4 +1,6 @@
+import os
 import psycopg2
+from urllib.parse import urlparse
 from psycopg2.extras import RealDictCursor
 from db.connection import get_db_config
 
@@ -6,13 +8,27 @@ from db.connection import get_db_config
 def get_conn():
     cfg = get_db_config()
 
-    conn = psycopg2.connect(
-        dbname=cfg['dbname'],
-        user=cfg['user'],
-        password=cfg['password'],
-        host=cfg.get('host', 'localhost'),
-        port=cfg.get('port', '5432')
-    )
+    db_url = os.getenv("DATABASE_URL")
+
+    if db_url:
+        result = urlparse(db_url)
+
+        conn = psycopg2.connect(
+            dbname=result.path[1:],
+            user=result.username,
+            password=result.password,
+            host=result.hostname,
+            port=result.port
+        )
+    else:
+        conn = psycopg2.connect(
+            dbname=cfg['dbname'],
+            user=cfg['user'],
+            password=cfg['password'],
+            host=cfg.get('host', 'localhost'),
+            port=cfg.get('port', '5432')
+        )
+
     return conn
 
 def get_cursor_dict(conn):
